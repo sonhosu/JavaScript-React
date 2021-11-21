dc067f070578daf9a5cc81d46cce4959a6d495b5
sonhosu <ghtn9398@nate.com>
2021-11-21 22:31:34 +0900

    검색 입력폼과 결과 리셋버튼 추가

--- js/Controller.js
+++ js/Controller.js
@@ -29,7 +29,11 @@ export default class Controller {
     this.store.search(searchKeyword)
     this.render()
   }
+  // 검색 폼에 x버튼 누를시 뷰와 검색어 리셋
   reset(){
+    this.store.searchKeyword=""
+    this.store.searchResult= []
+    this.render()
 
   }
   
